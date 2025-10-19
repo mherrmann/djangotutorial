f3e230ae6f06e0a065b92e40ff95aaf807db707c
Michael Herrmann <michael@herrmann.io>
2025-10-19 18:04:12 +0200

    Set SQLite file path to env["SQLITE_DB_FILE"]

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,7 +77,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.getenv('SQLITE_DB_FILE') or BASE_DIR / 'db.sqlite3',
     }
 }
 
