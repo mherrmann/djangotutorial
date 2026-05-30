e5e576dcb9959565dae392d9386fcc51652134cd
Michael Herrmann <michael@herrmann.io>
2026-05-30 10:00:53 +0200

    Add to settings.py

--- testproject/settings.py
+++ testproject/settings.py
@@ -127,3 +127,4 @@ DEBUG = os.getenv('DEBUG') == 'True'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(' ')
 INSTALLED_APPS += ['testapp']
 DATABASES['default']['NAME'] = os.getenv('SQLITE_DB_FILE') or DATABASES['default']['NAME']
+DATABASES['default']['NAME'] = os.getenv('SQLITE_DB_FILE') or DATABASES['default']['NAME']
