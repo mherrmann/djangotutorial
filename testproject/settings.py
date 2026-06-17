a84c8911b13accadc8068f83f6c1897ac7612046
Michael Herrmann <michael@herrmann.io>
2026-06-17 06:42:24 +0200

    Add to settings.py

--- testproject/settings.py
+++ testproject/settings.py
@@ -132,3 +132,10 @@ DATABASES['default'] = {
     'ENGINE': 'django.db.backends.sqlite3',
     'NAME': os.getenv('SQLITE_DB_FILE')
 }
+DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'djevopstest1781671106',
+    'USER': 'djevopstest1781671106',
+    'PASSWORD': os.environ['DB_PASSWORD'],
+    'HOST': 'localhost',
+}
