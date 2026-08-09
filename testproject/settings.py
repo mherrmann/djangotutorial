00f65e851d612589b3da9872fdbd9fa55e29c09e
Michael Herrmann <michael@herrmann.io>
2026-08-09 14:23:45 +0200

    Add to settings.py

--- testproject/settings.py
+++ testproject/settings.py
@@ -134,3 +134,10 @@ CELERY_BEAT_SCHEDULE = {
         'schedule': 5.0,
     },
 }
+DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'djevopstest1786277878',
+    'USER': 'djevopstest1786277878',
+    'PASSWORD': os.environ['DB_PASSWORD'],
+    'HOST': 'localhost',
+}
