fffd36dc6559b4e0f5e47f3d799de15334d2807e
Michael Herrmann <michael@herrmann.io>
2026-08-20 08:15:58 +0200

    Add to settings.py

--- testproject/settings.py
+++ testproject/settings.py
@@ -141,3 +141,7 @@ DATABASES['default'] = {
     'PASSWORD': os.environ['DB_PASSWORD'],
     'HOST': 'localhost',
 }
+DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.getenv('SQLITE_DB_FILE')
+}
